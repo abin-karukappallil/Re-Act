6ee3ba35ef36a1535db8c226ceae90e69b465ca8
Abin Karukappallil <followgamer102@gmail.com>
2024-03-13 21:48:25 +0530

    form css

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -2,10 +2,12 @@ function Form() {
   return (
     <div>
         <form action="post" className="form">
+          <div className="label">
           <label htmlFor="height">Height</label>
           <input type="number" />
           <label htmlFor="weight">Weight</label>
           <input type="number" />
+          </div>
           <button>Submit</button>
         </form>
     </div>
