05a07b958c07d04073852084d0cfd7777e144e2b
Abin Karukappallil <followgamer102@gmail.com>
2024-03-13 21:28:47 +0530

    form

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -1,7 +1,13 @@
 function Form() {
   return (
     <div>
-        
+        <form action="post">
+          <label htmlFor="height">Height</label>
+          <input type="number" />
+          <label htmlFor="weight">Weight</label>
+          <input type="number" />
+          <button>Submit</button>
+        </form>
     </div>
   )
 }
