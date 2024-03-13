45302a721919006fcfe7d1468d79bbed9446e3a8
Abin Karukappallil <followgamer102@gmail.com>
2024-03-13 22:22:55 +0530

    input script

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -2,20 +2,26 @@ import { useState } from "react";
 function Form() {
   const [weight, setWeight] = useState("");
   const [height, setHeight] = useState("");
+  const getWeight = (e) => {
+    console.log(e.target);
+    setWeight(e.target.value);
+
+
+  };
   return (
     <div>
-        <div className="form-wrapper">
+      <div className="form-wrapper">
         <form action="post" className="form">
           <h1>BMI calculator</h1>
           <div className="label">
-          <label htmlFor="height">Height(m)</label>
-          <input type="number" />
-          <label value={weight} onChange={(e)=> setWeight(e.target.value)} htmlFor="weight">Weight(kg)</label>
-          <input type="number" />
+            <label htmlFor="height">Height(m)</label>
+            <input type="number" />
+            <label  htmlFor="weight">Weight(kg)</label>
+            <input value={weight} onChange={getWeight} type="text" />
           </div>
           <button>Submit</button>
         </form>
-        </div>
+      </div>
     </div>
   )
 }
