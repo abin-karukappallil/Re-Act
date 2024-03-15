15717f9d453825cd2348b47437760ccb1e184675
Abin Karukappallil <followgamer102@gmail.com>
2024-03-16 00:08:45 +0530

    .

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -22,21 +22,19 @@ function Form() {
   //   alertMessage = '';
   // }
   return (
-    <div>
       <div className="form-wrapper">
         <form onSubmit={onSubmit} className="form">
           <h1>BMI calculator</h1>
           <div className="label">
             <label>Height(m)</label>
             <input value={height} onChange={(e) => { setHeight(e.target.value) }} type="text" required />
-            <label>Weight(kg)</label>
+            <label>Weigt(kg)</label>
             <input value={weight} onChange={(e) => { setWeight(e.target.value) }} type="text" required />
           </div>
           <button>Submit</button>
           {alert && <div className="alert" role="alert">Please Enter A Valid Input</div>}
         </form>
       </div>
-    </div>
   )
 }
 
