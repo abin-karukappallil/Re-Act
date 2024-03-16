876de13a8a0b11692de6978400a31dc1ef704dbf
Abin Karukappallil <followgamer102@gmail.com>
2024-03-16 20:26:06 +0530

    css changed

--- src/components/BmiScore.jsx
+++ src/components/BmiScore.jsx
@@ -1,8 +1,16 @@
 function BmiScore({bmino,bminame}) {
   return (
     <div>
-        <div>Hi {bmino} <br />
-        {bminame}</div>
+       <div className="main-score">
+        <div className="score-wrap">
+          <p>Your BMI Score</p>
+          <div className="square-score">
+            <h2>35.23</h2>
+          </div>
+          <h3>Over Weight</h3>
+          <h5>"You need to lose 4.5Kg"</h5>
+        </div>
+       </div>
     </div>
   )
 }
