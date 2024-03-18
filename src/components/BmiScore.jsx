ebfa19c7519ce56a61335c024c1f967371bbebec
Abin Karukappallil <followgamer102@gmail.com>
2024-03-18 20:23:15 +0530

    added basic styles

--- src/components/BmiScore.jsx
+++ src/components/BmiScore.jsx
@@ -1,16 +1,19 @@
-function BmiScore({bmino,bminame}) {
+function BmiScore({ bmino, bminame }) {
   return (
     <div>
-       <div className="main-score">
-        <div className="score-wrap">
-          <p>Your BMI Score</p>
-          <div className="square-score">
-            <h2>35.23</h2>
+      <div className="main-score">
+       
+          <div className="shadow">
+          <div className="score-wrap">
+            <p>Your BMI Score</p>
+            <div className="square-score">
+              <h2>35.23</h2>
+            </div>
+            <h3>Over Weight</h3>
+            <h5>"You need to lose 4.5Kg"</h5>
           </div>
-          <h3>Over Weight</h3>
-          <h5>"You need to lose 4.5Kg"</h5>
         </div>
-       </div>
+      </div>
     </div>
   )
 }
